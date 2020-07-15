1bea9b84459282f9f6446d10766d35670dba46e7
Ryan Beaumont <rbe72@uclive.ac.nz>
2020-07-15 12:05:39 +1200

    Added navbar component to the homepage

---
+++ personal-site/src/Desktop/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Navbar></Navbar>
+    <div style="height: 2000px"></div>
+  </div>
+</template>
+
+<script>
+  import Navbar from "./components/Navbar.vue";
+  export default {
+      name: "Home",
+      components: {
+          Navbar
+      }
+  }
+</script>
