b6b49c55e982ed957006736a1ad584a84e6dfa3e
Ryan Beaumont <ryan.beaumont1@gmail.com>
2020-07-15 17:22:31 +1200

    Initialised footer component for potential future use

---
+++ personal-site/src/Desktop/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-primary border border-dark sticky">
+    <p class="mx-auto">Ryan Beaumont</p>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+
+</style>
