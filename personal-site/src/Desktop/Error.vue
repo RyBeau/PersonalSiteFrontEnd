e7705277201188a05b4be686ecb7dcb06be1daca
Ryan Beaumont <ryan.beaumont1@gmail.com>
2020-07-15 20:45:48 +1200

    Created the Error.vue for fallback

---
+++ personal-site/src/Desktop/Error.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Navbar></Navbar>
+    <div class="jumbotron mb-0 vh-100 bg-light text-center">
+      <h1> Oops, an error has occurred</h1>
+      <p>You'll be redirected to the homepage shortly</p>
+      <hr class="my-4">
+      <div class="spinner-grow text-danger"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+    import Navbar from "./components/Navbar";
+    export default {
+      name: "Error",
+      components: {Navbar},
+      methods:{
+        redirect: function () {
+          setTimeout(() => {
+            this.$router.push("/");
+          }, 3500);
+        }
+      },
+    mounted: function () {
+      this.redirect()
+    }
+    }
+</script>
