9ab3f99ecb8983979812886f51f7c0ef1b8bdb83
Ryan Beaumont <ryan.beaumont1@gmail.com>
2020-07-15 20:45:31 +1200

    Setup https, removed uglifyjs

--- personal-site/webpack.config.js
+++ personal-site/webpack.config.js
@@ -49,6 +49,7 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     noInfo: true,
+    https:true,
     overlay: true
   },
   performance: {
@@ -61,18 +62,13 @@ module.exports = {
 }
 
 if (process.env.NODE_ENV === 'production') {
+  module.exports.optimization = {minimize: true};
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
       }
     }),
     new webpack.LoaderOptionsPlugin({
