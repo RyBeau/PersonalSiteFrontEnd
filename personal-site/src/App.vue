cf8539bad92a8cd3ef010fab8f69b1a45850944a
Ryan Beaumont <rbe72@uclive.ac.nz>
2020-07-16 12:07:48 +1200

    Advanced fade transition added

--- personal-site/src/App.vue
+++ personal-site/src/App.vue
@@ -1,12 +1,37 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <navbar></navbar>
+    <transition name="fade" mode="out-in" @before-leave="beforeLeave" @enter="enter" @after-enter="afterEnter">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
+import Navbar from "./Desktop/components/Navbar";
 export default {
-  name: 'app'
+  name: 'app',
+  components: {Navbar},
+  data() {
+    return {
+      previousHeight: 0
+    }
+  },
+  methods: {
+    beforeLeave: function (element) {
+      this.previousHeight = getComputedStyle(element.height);
+    },
+    enter: function (element) {
+      const {height} = getComputedStyle(element);
+      element.style.height = this.previousHeight;
+      setTimeout(() => {
+        element.style.height = height;
+      });
+    },
+    afterEnter: function (element) {
+      element.style.height = 'auto';
+    }
+  }
 }
 
 </script>
