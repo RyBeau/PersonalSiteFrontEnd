aba14c33f876cda9dc471220dccf18f1cf64704d
Ryan Beaumont <rbe72@uclive.ac.nz>
2020-07-16 12:45:56 +1200

    Added required fields

--- personal-site/src/Desktop/Contact.vue
+++ personal-site/src/Desktop/Contact.vue
@@ -5,23 +5,28 @@
         <h4 class="card-title">Contact Me</h4>
       </div>
       <div class="card-body">
-        <div class="form-group">
-          <label for="contactName">Name</label>
-          <input type="text" class="form-control shadow-sm" id="contactName">
-        </div>
-        <div class="form-group">
-          <label for="contactEmail">Email</label>
-          <input type="email" class="form-control shadow-sm" id="contactEmail">
-        </div>
-        <div class="form-group">
-          <label for="contactPhone">Phone</label>
-          <input type="tel" class="form-control shadow-sm" id="contactPhone">
-        </div>
-        <div class="form-group">
-          <label for="contactMessage">Message</label>
-          <textarea rows="5" class="form-control shadow-sm" id="contactMessage"></textarea>
-        </div>
-        <button class="btn btn-primary shadow">Submit</button>
+        <form>
+          <div class="form-group">
+            <label for="contactName">Name</label>
+            <input type="text" class="form-control shadow-sm" id="contactName" required>
+            <small class="form-text text-muted">Required</small>
+          </div>
+          <div class="form-group">
+            <label for="contactEmail">Email</label>
+            <input type="email" class="form-control shadow-sm" id="contactEmail" required>
+            <small class="form-text text-muted">Required</small>
+          </div>
+          <div class="form-group">
+            <label for="contactPhone">Phone</label>
+            <input type="tel" class="form-control shadow-sm" id="contactPhone">
+          </div>
+          <div class="form-group">
+            <label for="contactMessage">Message</label>
+            <textarea rows="5" class="form-control shadow-sm" id="contactMessage" required></textarea>
+            <small class="form-text text-muted">Required</small>
+          </div>
+          <button type=submit class="btn btn-primary shadow">Submit</button>
+        </form>
       </div>
     </div>
   </div>
