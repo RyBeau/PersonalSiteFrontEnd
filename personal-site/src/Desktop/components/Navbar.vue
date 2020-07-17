cc615cf501995d6fbfc8d6f1e95220c20e816be8
Ryan Beaumont <ryan.beaumont1@gmail.com>
2020-07-17 19:47:27 +1200

    added log out button

--- personal-site/src/Desktop/components/Navbar.vue
+++ personal-site/src/Desktop/components/Navbar.vue
@@ -22,14 +22,20 @@
         <router-link v-else class="nav-link" :to="{name: 'Profile'}">Profile</router-link>
       </li>
     </ul>
-    <button tag="button" class="btn btn-outline-light ml-md-auto" data-toggle="modal" data-target="#loginModal">Log In</button>
+    <button v-if="!loggedIn" tag="button" class="btn btn-outline-light ml-md-auto" data-toggle="modal" data-target="#loginModal">Log In</button>
+    <button v-else tag="button" class="btn btn-outline-light ml-md-auto">Log Out</button>
   </nav>
 </template>
 
 <script>
     import Login from "./login";
     export default {
-        name: "Navbar",
-      components: {Login}
+      name: "Navbar",
+      components: {Login},
+      data() {
+        return {
+          loggedIn: false
+        }
+      }
     }
 </script>
