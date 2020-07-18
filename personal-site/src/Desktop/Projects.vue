53c9981bbcc2a6c3602ccf24f6b3bc79e1189b07
Ryan Beaumont <ryan.beaumont1@gmail.com>
2020-07-18 20:55:37 +1200

    Built version 1 of the layout for the Projects.vue page

--- personal-site/src/Desktop/Projects.vue
+++ personal-site/src/Desktop/Projects.vue
@@ -1,11 +1,87 @@
 <template>
-  <div>
+  <div class="row mx-auto container p-5">
+    <div class="col- p-4 shadow overflow-auto border" style="height: 82vh">
+          <div v-for="(project, position) in projects" class="card mb-3 mx-auto project-card">
+            <div class="card-body position-absolute">
+              <h4 class="card-title">{{project.title}}</h4>
+              <h6 class="card-subtitle text-muted">{{project.subtitle}}</h6>
+              <h6>Author: {{project.username}}</h6>
+            </div>
+            <a ref="#" v-on:click="updateDisplayed(position)" class="stretched-link"></a>
+          </div>
+    </div>
+    <div class="col-lg overflow-auto shadow border p-5">
+      <h1 id="projectViewTitle"></h1>
+      <h6 id="projectViewAuthor"></h6>
+      <h6><a id="projectViewRepo" href="">Repository</a></h6>
+      <p id="projectViewDesc"></p>
+    </div>
   </div>
 </template>
 
 <script>
     export default {
         name: "Projects",
+      data() {
+        return {
+          projects: [
+            {
+              title: "Test Project 1",
+              description: "This is a test project for design the front end layout of the website." +
+                "The text here is for nothing more than helping choose the correct layout for the project page.",
+              repository: "www.github.com/rybeau",
+              username: "RyBeau"
+            },
+            {
+              title: "Test Project 2",
+              description: "This is a test project for design the front end layout of the website." +
+                "The text here is for nothing more than helping choose the correct layout for the project page.",
+              repository: "www.github.com/rybeau",
+              username: "RyBeau"
+            },
+            {
+              title: "Test Project 3",
+              description: "This is a test project for design the front end layout of the website." +
+                "The text here is for nothing more than helping choose the correct layout for the project page.",
+              repository: "www.github.com/rybeau",
+              username: "RyBeau"
+            },
+            {
+              title: "Test Project 4",
+              description: "This is a test project for design the front end layout of the website." +
+                "The text here is for nothing more than helping choose the correct layout for the project page.",
+              repository: "www.github.com/rybeau",
+              username: "RyBeau"
+            },
+            {
+              title: "Test Project 5",
+              description: "This is a test project for design the front end layout of the website." +
+                "The text here is for nothing more than helping choose the correct layout for the project page.",
+              repository: "www.github.com/rybeau",
+              username: "RyBeau"
+            },
+            {
+              title: "Test Project 6",
+              description: "This is a test project for design the front end layout of the website." +
+                "The text here is for nothing more than helping choose the correct layout for the project page.",
+              repository: "www.github.com/rybeau",
+              username: "RyBeau"
+            }
+          ]
+        }
+      },
+      mounted() {
+          this.updateDisplayed(0);
+      },
+      methods: {
+          updateDisplayed: function (position) {
+            document.getElementById("projectViewTitle").innerText = this.projects[position].title;
+            document.getElementById("projectViewAuthor").innerText = this.projects[position].username;
+            document.getElementById("projectViewRepo").setAttribute("href", "https://" +
+              this.projects[position].repository);
+            document.getElementById("projectViewDesc").innerText = this.projects[position].description;
+          }
+      }
     }
 </script>
 
